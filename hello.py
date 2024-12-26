e328944f428180cc95e2bcb4e8f4a96906f964d8
demo.labs08 <demo.labs08@gmail.com>
2024-12-26 13:15:49 +0530

    hello.py modified

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!!!"
+    return "It's Lunch time..empty stomach doesn't digest anything...especially study!!!"
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', debug=True)
