995df5e65f9323b864a879bd6b7cbb75051ee2ff
demo.labs08 <demo.labs08@gmail.com>
2024-12-27 11:13:31 +0530

    hello.py modified

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "It's Lunch time..empty stomach doesn't digest anything...especially study!!!"
+    return "The Jenkins CICD pipeline is working!!!"
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', debug=True)
